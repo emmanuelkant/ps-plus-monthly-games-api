2ecb27e83decb1233663b32ff50976ccddbb4ea7
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2021-01-13 19:44:19 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a233786a95398facff0a3b7ae4f99729'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ab0955e0-be01-4b64-89f5-d2accf02a73c'
+};
